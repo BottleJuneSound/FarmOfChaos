9c9139d3a1342b8f3887a0b78ea3f8e96ab3b95e
BottleJuneSound <kbjun0320@naver.com>
2024-11-01 10:27:42 +0900

    AddCodeComment
    enemy 스크립트 수정, 주석추가

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,13 +4,13 @@ using UnityEngine.Tilemaps;
 public class EnemyController : MonoBehaviour
 {
     public Vector3 currentPosition;
-    public Vector3 finishPosition = new Vector3(0,0,0);
+    public Vector3 finishPosition = new Vector3(0, 0, 0);
     public Vector3 resetPosition;
 
     public float speed = 2;
     public Collider2D rangeCollider;
     //public TilemapCollider2D waterCollider;
-
+    bool moveResetZone;
 
 
 
@@ -18,56 +18,67 @@ public class EnemyController : MonoBehaviour
     {
 
         currentPosition = transform.position;
-        resetPosition = new Vector2(Random.Range(-1,1.9f), Random.Range(-10, -11));
+        resetPosition = new Vector2(Random.Range(-1, 1.9f), Random.Range(-10, -11));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(currentPosition != finishPosition)
+        currentPosition = transform.position;
+
+        //목적지로 이동
+        if (currentPosition != finishPosition)
         {
-            GoFinishZone();
-      
+            if (moveResetZone == false) //중첩 if를 활용하여 리셋존 이동중이라면 작동하지 않음
+            {
+                GoFinishZone();
+            }
         }
 
+        //object 태그 충돌이 충족하면 시작
+        if (moveResetZone == true)
+        {
+            float resetSpeed = 2;
+            Debug.Log("리셋포인트로 이동!");
 
+            transform.position = Vector3.MoveTowards
+                (transform.position, resetPosition, resetSpeed * Time.deltaTime);
+        }
+
+        //리셋포인트에 도착하면 정지
+        if (resetPosition == currentPosition)
+        {
+            moveResetZone = false;
+            Debug.Log("리셋포인트에 도착했습니다.");
+        }
     }
 
-    public void GoFinishZone()
+    public void GoFinishZone()  //목적지로 이동하는 메서드
     {
-        currentPosition = transform.position;
-
-
+        speed = 2f;
         transform.position = Vector3.MoveTowards
             (transform.position, finishPosition, speed * Time.deltaTime);
+        Debug.Log("목적지로 이동중!");
 
-
-        // 이동 후 위치를 업데이트하여 currentPosition을 새로 설정
-        currentPosition = transform.position;
     }
 
-    public void StopMove()
+    public void StopMove()  //정지를 관리하는 메서드
     {
-
         speed = 0;
+        Debug.Log("충돌");
+
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)  //콜라이더 충돌관리
     {
         currentPosition = transform.position;
-
 
         if (collision.gameObject.tag == "Object")
         {
             StopMove();
+            moveResetZone = true;
 
-            float resetSpeed = 2;
-
-            Debug.Log("충돌");
-            currentPosition = Vector3.MoveTowards
-                (currentPosition, resetPosition, resetSpeed *Time.deltaTime);
         }
 
-        
+
     }
 }
